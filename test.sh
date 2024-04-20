cdeebff8f90f1d557acff2e784bb6d86e26327d1
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-20 02:01:27 -0500

    full train test

--- test.sh
+++ test.sh
@@ -10,10 +10,9 @@
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw7
-#SBATCH --array=0
 
 . /home/fagg/tf_setup.sh
 conda activate dnn_2024_02
 
 
-python hw7_base.py -vv @exp.txt @oscer.txt @discriminator.txt @generator.txt @meta.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK
+python hw7_base.py -vv @exp.txt @oscer.txt @discriminator.txt @generator.txt @meta.txt --cpus_per_task $SLURM_CPUS_PER_TASK
