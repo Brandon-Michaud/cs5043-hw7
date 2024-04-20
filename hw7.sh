bb3ec2408d237e0792f693a4f40b8189db9d2c74
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-20 01:32:07 -0500

    full train test

--- hw7.sh
+++ hw7.sh
@@ -11,11 +11,10 @@
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw7
-#SBATCH --array=0
 
 . /home/fagg/tf_setup.sh
 conda activate dnn_2024_02
 module load cuDNN/8.9.2.26-CUDA-12.2.0
 
 
-python hw7_base.py -vv --gpu @exp.txt @oscer.txt @discriminator.txt @generator.txt @meta.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK
+python hw7_base.py -vv --gpu @exp.txt @oscer.txt @discriminator.txt @generator.txt @meta.txt --cpus_per_task $SLURM_CPUS_PER_TASK
